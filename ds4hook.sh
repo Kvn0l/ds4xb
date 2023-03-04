e8ba15c9b12d02f846b57491b5f0f72f239a9ac2
Kvn0l <kevinlo11@proton.me>
2023-03-04 17:48:26 -0300

    Test fix

--- ds4hook.sh
+++ ds4hook.sh
@@ -4,4 +4,4 @@ kill -9 $(cat /tmp/ds4hook.pid)
 rm /tmp/ds4hook.pid
 
 echo $$ > /tmp/ds4hook.pid
-evdevhook /usr/local/etc/ds4.json
+evdevhook /etc/ds4hook.json
--- ds4xb.sh
+++ ds4xb.sh
@@ -5,4 +5,4 @@ rm /tmp/ds4xb.pid
 
 echo $$ > /tmp/ds4xb.pid
 EVNUM=`grep 'Name="Wireless Controller"' -A 4 /proc/bus/input/devices | tail -1 | sed -E 's/^.*event([0-9]+)\s.*$/\1/'`
-[ ! -z "$EVNUM" ] && xboxdrv --evdev "/dev/input/event${EVNUM}" --config /usr/local/etc/ds4xb.xboxdrv
+[ ! -z "$EVNUM" ] && xboxdrv --evdev "/dev/input/event${EVNUM}" --config /etc/ds4xb.xboxdrv
